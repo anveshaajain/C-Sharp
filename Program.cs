887acf55f7fc9e57605c0009a35778dca7dc8611
Anvesha jain <anveshajain1436@gmail.com>
2025-08-14 10:20:03 +0530

    added C# file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class lucky_ticket
+{
+    static void Main()
+    {
+        for(int i=1; i <=100; i++)
+            if (i%12==0)
+                Console.WriteLine("Lucky tickets are: "+ i);
+    }
+}
