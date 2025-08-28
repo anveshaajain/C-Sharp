634b08b8ec6b8f2e499f7fc9320d40dcb36c2fe8
Anvesha jain <anveshajain1436@gmail.com>
2025-08-28 09:42:33 +0530

    added course file

---
+++ course/course/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Course
+{
+    protected void Showdetails()
+    {
+        Console.WriteLine("showing course details");
+    }
+}
+class OnlineCourse : Course
+{
+    public void display ()
+    {
+        Showdetails();
+    }
+}
+class program
+{
+    static void Main()
+    {
+        var onlineCourse =  new OnlineCourse();
+        onlineCourse.display();
+    }
+}
